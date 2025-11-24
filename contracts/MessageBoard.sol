e4f18eb2b9e816edb95d499e182bdf43a4d215c8
Dababah <pawasalee96@gamil.com>
2025-11-24 08:29:50 +0700

    first commit

---
+++ contracts/MessageBoard.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+contract MessageBoard {
+    string public message;
+
+    event MessageUpdated(address indexed sender, string newMessage);
+
+    function setMessage(string memory _msg) public {
+        message = _msg;
+        emit MessageUpdated(msg.sender, _msg);
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+}
